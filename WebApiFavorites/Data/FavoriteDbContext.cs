7e77e8af0cda11bc0a331b1a95fa43704f2a6809
Didi <demi.torrealba@hotmail.com>
2024-11-26 14:51:16 +0100

    new api

---
+++ WebApiFavorites/Data/FavoriteDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApiFavorites.Data
+{
+    public class FavoriteDbContext
+    {
+    }
+}
---
+++ WebApiFavorites/Models/Favorite.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiFavorites.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? ProductId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
